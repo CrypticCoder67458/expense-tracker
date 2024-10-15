4f1a3b1cecead3eba8eb85f34e2f73c29334a7db
mlikayoussef <youssefmlika.lac2@gmail.com>
2024-10-15 13:48:38 +0200

    tutorial setup

---
+++ tracker/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import '../style.css'
+import AddTransaction from './components/Add Transaction.vue';
+import Balance from './components/Balance.vue';
+import Header from './components/Header.vue'
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionsList from './components/TransactionsList.vue';
+
+const income=ref (0);
+const expense=ref(0);
+const transactions=ref([]);
+
+</script>
+
+<template>
+  <div class="container">
+    <Header />
+    <Balance />
+    <IncomeExpense />
+    <TransactionsList />
+    <AddTransaction />
+
+  </div>
+  
+</template>
+
